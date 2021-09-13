b22d4263198f293240b6e90deadd8f7b3c29fc4b
Hacker Liar <46444347+hackerliar@users.noreply.github.com>
2021-09-13 22:29:15 +0700

    Update themescode.js

--- themescode.js
+++ themescode.js
@@ -1,37 +1,3 @@
-	(function()
-	{
-		if(typeof(window.getComputedStyle) == 'undefined') 
-		{ 
-			return; 
-		}
-		
-		var pre = document.getElementsByTagName('pre');
-		
-		for(var len = pre.length, i = 0; i < len; i ++)
-		{
-			var code = pre[i].getElementsByTagName('code').item(0);
-			if(!code)
-			{
-				code = pre[i].getElementsByTagName('samp').item(0);
-				if(!code)
-				{
-					continue;
-				}
-			}
-			
-			var column = document.createElement('div');
-			column.setAttribute('aria-hidden', 'true');
-			
-			for(var n = 0; n < code.innerHTML.split(/[\n\r]/g).length; n ++)
-			{
-				column.appendChild(document.createElement('span'));
-			}
-			
-			pre[i].insertBefore(column, code);
-			pre[i].className = 'line-numbers';
-		}	
-	
-	})();
 var _self = "undefined" != typeof window ? window : "undefined" != typeof WorkerGlobalScope && self instanceof WorkerGlobalScope ? self : {},
     Prism = function(u) {
         var c = /\blang(?:uage)?-([\w-]+)\b/i,
